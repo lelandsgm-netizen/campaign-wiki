5d5922afa902e0709571c0169807a458db343328
lelandsgm-netizen <lelandsgm@gmail.com>
2026-05-26 09:57:05 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Loreforge Works",
+    pageTitle: "LoreForge Works",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
